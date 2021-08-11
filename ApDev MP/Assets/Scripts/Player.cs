2e37a4d29ee2f09d366159f802a2b3920e976210
JunmaOgawa <junma_ogawa@dlsu.edu.ph>
2021-08-12 07:04:14 +0900

    General update to main
    changed model (temp)
    added movement and camera movement

--- ApDev MP/Assets/Scripts/Player.cs
+++ ApDev MP/Assets/Scripts/Player.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class Player : MonoBehaviour
 {
+    private Transform playerModel;
+
+    [Space]
+
+    [Header("Parameters")]
+    public float xySpeed = 18;
+    public float lookSpeed = 340;
+    public float forwardSpeed = 6;
+
+    [Space]
+
+    [Header("Public References")]
+    public Transform aimTarget;
+    public CinemachineDollyCart dolly;
+    public Transform cameraParent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerModel = transform.GetChild(0);
+        SetSpeed(forwardSpeed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+
+        LocalMove(h, v, xySpeed);
+        RotationLook(h, v, lookSpeed);
+        HorizontalLean(playerModel, h, 80, .1f);
+    }
+
+    void LocalMove(float x, float y, float speed)
+    {
+        transform.localPosition += new Vector3(x, y, 0) * speed * Time.deltaTime;
+        ClampPosition();
+    }
+    void ClampPosition()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+
+    void RotationLook(float h, float v, float speed)
+    {
+        aimTarget.parent.position = Vector3.zero;
+        aimTarget.localPosition = new Vector3(h, v, 1);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(aimTarget.position), Mathf.Deg2Rad * speed * Time.deltaTime);
+    }
+    
+    void HorizontalLean(Transform target, float axis, float leanLimit, float lerpTime)
+    {
+        Vector3 targetEulerAngels = target.localEulerAngles;
+        target.localEulerAngles = new Vector3(targetEulerAngels.x, targetEulerAngels.y, Mathf.LerpAngle(targetEulerAngels.z, -axis * leanLimit, lerpTime));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(aimTarget.position, .5f);
+        Gizmos.DrawSphere(aimTarget.position, .15f);
+
+    }
+
+    void SetSpeed(float x)
+    {
+        dolly.m_Speed = x;
     }
 }
