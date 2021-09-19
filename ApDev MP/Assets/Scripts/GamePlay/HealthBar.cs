632d688a752747e1e139b7aa457f5feba4bcdbc2
JunmaOgawa <junma_ogawa@dlsu.edu.ph>
2021-09-20 03:42:31 +0900

    boss hp

--- ApDev MP/Assets/Scripts/GamePlay/HealthBar.cs
+++ ApDev MP/Assets/Scripts/GamePlay/HealthBar.cs
@@ -7,14 +7,24 @@ public class HealthBar : MonoBehaviour
 {
     public Slider slider;
     public Player player;
+    public BossBehavior boss;
+    public BossMan bossMan;
 
     public void SetMaxHealth(int health)
     {
         slider.maxValue = player.HP;
         slider.value = player.HP;
+
+        slider.maxValue = boss.health;
+        slider.value = boss.health;
+
+        slider.maxValue = boss.health;
+        slider.value = bossMan.health;
     }
     public void SetHealth(int health)
     {
         slider.value = player.HP;
+        slider.value = boss.health;
+        slider.value = bossMan.health;
     }
 }
