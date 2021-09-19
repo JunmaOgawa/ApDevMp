ca68d878fb89066163bb3b5453771f810cfb015b
JunmaOgawa <junma_ogawa@dlsu.edu.ph>
2021-09-20 01:23:14 +0900

    bosses

--- ApDev MP/Assets/Scripts/Bosses/BossMan.cs
+++ ApDev MP/Assets/Scripts/Bosses/BossMan.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BossBehavior : MonoBehaviour
+public class BossMan : MonoBehaviour
 {
     //player reference
     public Player player;
@@ -12,14 +12,6 @@ public class BossBehavior : MonoBehaviour
     //parameters
     public float health;
     public int type; //0 = R, 1 = G, 2 = B
-    public Rigidbody projectile;
-
-    //color Materials
-    public Material RedMaterial;
-    public Material BlueMaterial;
-    public Material GreenMaterial;
-    //enemy rndererholder
-    Renderer rend;
 
     //attacking
     public float timeBetweenAttacks;
@@ -27,39 +19,23 @@ public class BossBehavior : MonoBehaviour
     public float attackRange;
     public bool playerInAttackRange;
 
-    //pointManager
-    //public PointManager pointManager;
+    public Animator animator;
 
     private void Awake()
     {
         playerTransform = GameObject.Find("GameplayPlane").transform;//find player transform
         //parameters
-        health = 40;//set health
-        type = 0;//set type
+        health = 80;//set health
+        type = 2;//set type
 
-        timeBetweenAttacks = 0.5f;
-        attackRange = 5000;
-
-        /*//change color to type
-        rend = GetComponent<Renderer>();
-        rend.enabled = true;
-        if (type == 0) //red
-        {
-            rend.sharedMaterial = RedMaterial;
-        }
-        else if (type == 1) //green
-        {
-            rend.sharedMaterial = GreenMaterial;
-        }
-        else if (type == 2) //blue
-        {
-            rend.sharedMaterial = BlueMaterial;
-        }*/
+        timeBetweenAttacks = 5f;
+        attackRange = 10000;
     }
 
     private void Update()
     {
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        //playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        playerInAttackRange = true;
         if (playerInAttackRange)
         {
             Debug.Log("Player is in range");
@@ -70,7 +46,6 @@ public class BossBehavior : MonoBehaviour
         {
             Destroy(gameObject);
             FindObjectOfType<AudioManager>().Play("Death");
-            // FindObjectOfType<PointManager>().AddPoints();
         }
     }
 
@@ -79,12 +54,7 @@ public class BossBehavior : MonoBehaviour
         transform.LookAt(playerTransform);
         if (!alreadyAttacked)
         {
-            //place attack code
-            //need to change for space dimentions
-            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
-
-            rb.AddForce(transform.forward * 10.0f, ForceMode.Impulse);
-
+            animator.SetTrigger("onAttack");
             alreadyAttacked = true;
             Invoke(nameof(ResetAttack), timeBetweenAttacks);
         }
@@ -116,6 +86,7 @@ public class BossBehavior : MonoBehaviour
             case "Blue":
                 if (type == 2) health -= 3;
                 else health--;
+                animator.SetTrigger("onDamaged");
                 Destroy(collision.gameObject);
                 break;
         }
